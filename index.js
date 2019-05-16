034c72390d465fa427cd7117699452e70da2ab98
PyroTechniac <39341355+PyroTechniac@users.noreply.github.com>
2019-05-15 20:30:17 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Client } = require('discord.js');
+const Raven = require('raven');
+require('dotenv').config();
+
+Raven.config(process.env.SENTRY, { captureUnhandledRejections: true }).install();
+
+const client = new Client()
+
+client.ws.on('GUILD_DELETE', (d) => {
+    Raven.captureMessage(d);
+    console.log(d);
+});
+
+client.once('ready', () => console.log('Ready'))
+
+client.login(process.env.TOKEN);
